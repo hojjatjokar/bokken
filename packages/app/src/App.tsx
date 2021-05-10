02e8e97c59089eb90f4b6e059a5f1fa5d0d0d5c8
Hojjat Jokar <paintthedream@gmail.com>
2021-05-10 21:18:12 +0330

    chore: add components to app

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@bokken/components';
 import Icon from './assets/images/bokken.svg';
 import './style.css';
 
@@ -5,6 +6,7 @@ const App = (): JSX.Element => (
   <div>
     <h1>Hello Bokken</h1>
     <img src={Icon} alt="bokken" />
+    <Button label="hojjat" />
   </div>
 );
 
