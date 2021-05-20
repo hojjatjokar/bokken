b2d7a13a49a5709374650ff323982aa9b0fd7506
Hojjat Jokar <paintthedream@gmail.com>
2021-05-20 08:01:14 +0330

    chore: add testing config to app

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import { Button } from '@bokken/components';
 import { noob } from '@bokken/utils';
-import Icon from './assets/images/bokken.svg';
-import './style.css';
+//import Icon from './assets/images/bokken.svg';
+// import './style.css';
 
 const App = (): JSX.Element => {
   noob();
   return (
     <div>
       <h1>Hello Bokken</h1>
-      <img src={Icon} alt="bokken" />
       <Button label="hojjat" />
     </div>
   );
